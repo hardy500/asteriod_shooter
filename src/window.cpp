bf23f62e2f132cffd04fbb5d6b6cdeb293bb7395
toto_wng <r.news94@gmail.com>
2023-06-24 20:03:11 +0200

    add movement

--- src/window.cpp
+++ src/window.cpp
@@ -4,6 +4,8 @@
 
 #define WINDOW_WIDTH  1280
 #define WINDOW_HEIGHT 720
+#define FPS 120
+#define FRAME_DELAY  (1000/FPS)
 
 enum Select { TTF, IMG };
 
@@ -86,19 +88,27 @@ int main() {
   SDL_Texture* texture_ship = texture_create(window, renderer, "assets/graphics/ship.png", img);
   SDL_Texture* texture_text = texture_create(window, renderer, "assets/graphics/subatomic.ttf", ttf);
 
-  SDL_Rect rect_text = rect_create(texture_text, 500, 200);
-  SDL_Rect rect_ship = rect_create(texture_ship, 300, 500);
+  SDL_Rect rect_text = rect_create(texture_text, (WINDOW_WIDTH/2 - 80), (WINDOW_HEIGHT-80));
+  SDL_Rect rect_ship = rect_create(texture_ship, (WINDOW_WIDTH/2 - 40), (WINDOW_HEIGHT/2));
 
   // ------------------------------------------------------------------------------------
+
+  Uint32 frame_start;
+  int frame_time;
 
   bool quit = false;
   SDL_Event event;
   while (!quit) {
+    frame_start = SDL_GetTicks();
+
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
         quit = true;
       }
     }
+
+
+    if (rect_ship.y > 0) rect_ship.y -= 4;
 
     SDL_RenderClear(renderer);
 
@@ -107,8 +117,11 @@ int main() {
     SDL_RenderCopy(renderer, texture_text, NULL, &rect_text);
 
     SDL_RenderPresent(renderer);
-  }
 
+    frame_time = SDL_GetTicks() - frame_start;
+    if (frame_time < FRAME_DELAY) {SDL_Delay(FRAME_DELAY - frame_time);}
+
+  }
   // ------------------------------------------------------------------------------------
 
   SDL_DestroyWindow(window);
